886892c8fed0ab99e1b78e4c38372adca65d1e06
Abhishek Nagar <nabhishek734@gmail.com>
2024-03-05 21:38:58 +0530

    Creating custom React

--- First/first1.js
+++ First/first1.js
@@ -1,1 +1,21 @@
-const Element = document.getElementById("root");
+function customRender(createElement,container){
+    const newElement = document.createElement(createElement.type);
+    newElement.innerHTML = createElement.children;
+
+    for (const prop in createElement.props){
+        newElement.setAttribute(prop,createElement.props[prop])
+    }
+
+    container.appendChild(newElement);
+}
+const createElement = {
+    type : 'a',
+    props : {
+        href : 'https:/Linkedin.com',
+        target : '_blank'
+    },
+    children : 'Go To Linkedin: '
+
+}
+const container = document.getElementById("root");
+customRender(createElement,container);
